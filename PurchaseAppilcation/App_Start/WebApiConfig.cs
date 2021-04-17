8e7590b8074130f31267c2ed6b431146a76eb064
Mohammed-Ameen-GitHub <82137797+Mohammed-Ameen-GitHub@users.noreply.github.com>
2021-04-17 12:02:04 +0530

    Add files via upload

---
+++ PurchaseAppilcation/App_Start/WebApiConfig.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using Microsoft.Owin.Security.OAuth;
+
+namespace PurchaseApplication
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+          
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "ControllersApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+      
+            var json = config.Formatters.JsonFormatter;
+            json.SerializerSettings.PreserveReferencesHandling = Newtonsoft.Json.PreserveReferencesHandling.Objects;
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+
+            json.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+
+        }
+    }
+}
---
+++ PurchaseAppilcation/Controllers/ItemMastersController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using System.Security.Claims;
+using PurchaseApplication.Models;
+
+namespace PurchaseApplication.Controllers
+{
+    [System.Web.Http.RoutePrefix("Api/ItemMasters")]
+    [Authorize]
+    public class ItemMastersController : ApiController
+    {
+        private PurchaseEntities db = new PurchaseEntities();
+
+        [ResponseType(typeof(ItemMaster))]
+        [System.Web.Http.Route("PostItemMaster")]
+        [System.Web.Http.HttpPost]
+        public IHttpActionResult PostItemMaster(ItemMaster itemMaster)
+        {
+            
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            using (db)
+            {
+                if (itemMaster.Id == 0)
+                {
+                    db.ItemMasters.Add(itemMaster);
+                    db.SaveChanges();
+                }
+                else
+                    return NotFound();
+            }
+            return Ok();
+        }
+
+      
+        [ResponseType(typeof(ItemMaster))]
+        [System.Web.Http.Route("DeleteItemMaster/{id}")]
+        [System.Web.Http.HttpDelete]
+        public IHttpActionResult DeleteItemMaster(int id)
+        {
+
+            ItemMaster itemMaster = db.ItemMasters.Find(id);
+
+            if (itemMaster == null)
+            {
+                return NotFound();
+            }
+
+            db.ItemMasters.Remove(itemMaster);
+            db.SaveChanges();
+
+            return Ok(itemMaster);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ItemMasterExists(int id)
+        {
+            return db.ItemMasters.Count(e => e.Id == id) > 0;
+        }
+    }
+}
